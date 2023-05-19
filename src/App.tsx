603323d67b52f0ab60b0d5ea720dc903a1fcab46
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-05-18 22:55:31 -0400

    dnd working, MUI autocomplete working with spotify api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from "react";
 import Album from "./components/Album";
-import { Autocomplete } from "@mui/material";
+import { Autocomplete, Button } from "@mui/material";
 import TextField from "@mui/material/TextField/TextField";
-/* import CircularProgress from "@mui/material"; */
 import { debounce } from "lodash";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 //interface
 type Album = {
@@ -15,10 +20,11 @@ type Album = {
 };
 
 function App() {
-  const [searchInput, setSearchInput] = useState<string>("Jimmy Hendrix");
+  const [searchInput, setSearchInput] = useState<string>("");
   const [albums, setAlbums] = useState<Album[]>([]);
+  const [albumList, setAlbumList] = useState<Album[]>([]);
+  const [selectedAlbum, setSelectedAlbum] = useState<Album[]>([]);
   const [accessToken, setAccessToken] = useState<string>("");
-  /*   const [loading, setLoading] = useState<boolean>(false); */
 
   //ENV
   const ID = import.meta.env.VITE_SPOT_CLIENT_ID;
@@ -41,6 +47,7 @@ function App() {
   }, [ID, SECRET]);
 
   //search
+
   const search = debounce(async () => {
     const albumParams = {
       method: "GET",
@@ -49,74 +56,136 @@ function App() {
         Authorization: `Bearer ${accessToken}`,
       },
     };
+    try {
+      const res = await fetch(
+        `https://api.spotify.com/v1/search?q=${searchInput}&type=album&limit=5`,
+        albumParams
+      );
+      const data = await res.json();
+      setAlbums(data?.albums?.items);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 1000);
 
-    await fetch(
-      `https://api.spotify.com/v1/search?q=${searchInput}&type=album&limit=10`,
-      albumParams
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setAlbums(data?.albums?.items);
+  //handle drag end
+  const handleDragEnd = (event: { active: any; over: any }) => {
+    const { active, over } = event;
+    if (active.id !== over.id) {
+      setAlbumList((items) => {
+        const activeIndex = items.findIndex((item) => item.id == active.id);
+        const overIndex = items.findIndex((item) => item.id == over.id);
+
+        return arrayMove(items, activeIndex, overIndex);
       });
-  }, 1500);
+    }
+  };
 
-  //handle input change
   const handleInputChange = (value: string) => {
     setSearchInput(value);
-    if (searchInput.trim().length > 1) {
+    if (searchInput.trim().length > 0) {
       search();
     }
-    console.log(albums);
   };
 
   //autocomplete options
-  const autoCompleteOptions = albums
-    ? albums.map((album) => ({
-        label: `${album.name} - ${album.artists[0].name}`,
-        key: album.id,
-        value: album.id,
-      }))
-    : [];
+  const optionsAc =
+    albums &&
+    albums.map((album) => ({
+      name: album.name,
+      artists: album.artists[0].name,
+      release_date: album.release_date.slice(0, 4),
+      images: album.images[0].url,
+      id: album.id,
+    }));
 
   return (
     <>
       <div className="main-container">
         <h1>Top Albums List</h1>
         <div className="input-container">
-          <input
-            type="text"
-            placeholder="Add an Album..."
-            onChange={(e) => handleInputChange(e.target.value)}
+          <Autocomplete
+            sx={{ width: 500, backgroundColor: "white", color: "white" }}
+            options={optionsAc}
+            filterOptions={(options) => options}
+            renderOption={(props, optionsAc) => (
+              <li {...props} key={optionsAc.id}>
+                <Album
+                  title={optionsAc.name}
+                  artist={optionsAc.artists}
+                  image={optionsAc.images}
+                  year={optionsAc.release_date}
+                  id={optionsAc.id}
+                ></Album>
+              </li>
+            )}
+            getOptionLabel={(options) => options.name}
+            isOptionEqualToValue={(option, value) => option.name === value.name}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Add an Album"
+                variant="outlined"
+                onChange={(e) => handleInputChange(e.target.value)}
+              />
+            )}
+            onChange={(_, value) => {
+              if (value !== null) {
+                const album = {
+                  name: value.name,
+                  artists: [{ name: value.artists }],
+                  release_date: value.release_date,
+                  images: [{ url: value.images }],
+                  id: value.id,
+                };
+                setSelectedAlbum([album]);
+              }
+            }}
           />
-
-          <button onClick={() => search()}>Add</button>
+          <Button
+            onClick={() =>
+              setAlbumList((previousState) => {
+                const albumExists = previousState.some((album) => {
+                  return album.name === selectedAlbum[0].name;
+                });
+                if (albumExists) {
+                  return [...previousState];
+                }
+                return [...previousState, ...selectedAlbum];
+              })
+            }
+            variant="contained"
+            color="primary"
+          >
+            Add Album
+          </Button>
         </div>
-
-        <Autocomplete
-          sx={{ width: 500, backgroundColor: "white" }}
-          options={autoCompleteOptions}
-          getOptionLabel={(options) => options.label}
-          isOptionEqualToValue={(option, value) => option.key === value.key}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Search"
-              variant="outlined"
-              onChange={(e) => handleInputChange(e.target.value)}
-            />
-          )}
-        />
-
-        {/*     {albums.map((album, i) => (
-          <Album
-            key={i}
-            title={album.name}
-            artist={album.artists[0].name}
-            year={album.release_date.slice(0, 4)}
-            image={album.images[0].url}
-            placement={i + 1}
-          />
-        ))} */}
+        <div className="list-container">
+          <DndContext
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={albumList.map((item) => item.id)}
+              strategy={verticalListSortingStrategy}
+            >
+              {albumList?.map((item, i) => (
+                <Album
+                  key={item.name}
+                  id={item.id}
+                  image={item.images[0].url}
+                  title={item.name}
+                  artist={item.artists[0].name}
+                  year={item.release_date.slice(0, 4)}
+                  placement={i + 1}
+                />
+                /*   <div key={i} id={item.id}>
+                  {item.name}
+                </div> */
+              ))}
+            </SortableContext>
+          </DndContext>
+        </div>
       </div>
     </>
   );
--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -1,16 +1,40 @@
 import AlbumCSS from "./Album.module.css";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface Props {
   image: string;
   title: string;
   artist: string;
   year: string;
-  placement: number;
+  placement?: number;
+  id: string;
 }
 
-const Album: React.FC<Props> = ({ image, title, artist, year, placement }) => {
+const Album: React.FC<Props> = ({
+  image,
+  title,
+  artist,
+  year,
+  placement,
+  id,
+}) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
   return (
-    <div className={AlbumCSS.main}>
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className={AlbumCSS.main}
+      data-id={id}
+    >
       <div className={AlbumCSS.image}>
         <div className={AlbumCSS.placement}>{placement}</div>
         <img src={image} alt="" height="75" />
